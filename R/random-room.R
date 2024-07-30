7d01887c3377dce712844faceee3c19379d26cb6
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-07-30 14:16:41 +0000

    Experiment with a randomised version of greedy2.

--- R/random-room.R
+++ R/random-room.R
@@ -1,4 +1,4 @@
-random_room <- function(order, seed) {
+random_room_1 <- function(order, seed) {
   
   r <- Room$new(size = order)
   
@@ -16,6 +16,28 @@ random_room <- function(order, seed) {
            n = as.integer(order),
     n_filled = as.integer(r$n_filled),
       volume = r$volume
+  )
+  
+}
+
+random_room_2 <- function(order, seed) {
+  
+  r <- Room$new(size = order)
+  
+  for(p in sample(r$free_pairs, length(r$free_pairs))) {
+    for(e in sample(r$empty_cells, length(r$empty_cells))) {
+      if(r$is_available(e, p)) {
+        r$set(e, p)
+        break()
+      }
+    }
+  }
+  
+  tibble(
+    seed = as.integer(seed),
+    n = as.integer(order),
+    n_filled = as.integer(r$n_filled),
+    volume = r$volume
   )
   
 }
